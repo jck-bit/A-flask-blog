abba7f66aa6e2b7b0c6af5a548f2a58a66de1283
jck-bit <jckkinyanjui@gmail.com>
2022-08-29 19:11:18 +0300

    Users can now reset their password

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -62,5 +62,25 @@ class UpdateAccountForm(FlaskForm):
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('content', validators=[DataRequired()])
+    content = TextAreaField('content', validators=[DataRequired(), FileAllowed(['jpeg' ,'png' ,'jpg'])])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+     email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+     submit = SubmitField('Request Password Reset')
+
+
+     def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email...Register First')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
